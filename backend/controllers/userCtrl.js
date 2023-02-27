a71317a867efb9dcf806adffffcfd0f4b99a5f70
nguyentuan1906 <roccat.nguyentuan@gmail.com>
2023-02-27 15:17:37 +0700

    a

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -14,18 +14,18 @@ const userCtrl = {
             const newUser = await User({ email, password: passwordHash, name });
             await newUser.save();
             // res.json({newUser});
-            res.json({ msg: "Register complete!"});
+            res.json({ msg: "Register complete!" });
         } catch (error) {
             return res.status(500).json({ msg: error.message });
         }
     },
     accessToken: (req, res) => {
         try {
-            const rf_token = req.cookies.refreshtoken;
+            const acc_token = req.cookies.accesstoken;
             //Chưa đăng nhập
-            if (!rf_token)
+            if (!acc_token)
                 return res.status(400).json({ msg: "Chua dang nhap!" }); //Trả về thông báo chưa đăng nhập
-            jwt.verify(rf_token, process.env.REFRESH, (err, user) => {
+            jwt.verify(acc_token, process.env.ACCESS, (err, user) => {
                 if (err)
                     return res.status(400).json({ msg: "Chua dang nhap!" });
                 const accesstoken = createAccessToken({ id: user._id });
@@ -44,20 +44,20 @@ const userCtrl = {
             const isWatch = await bcrypt.compare(password, user.password);
             if (!isWatch)
                 return res.status(400).json({ msg: "incorrect password" });
-            const accesstoken = createAccessToken({ id: user._id });
             // const refreshtoken = createRefreshToken({ id: user._id });
+            const accesstoken = createAccessToken({ id: user._id });
             res.cookie("accesstoken", accesstoken, {
                 httpOnly: true,
                 path: "/user/accesstoken",
             });
-            res.json({ accesstoken});
+            res.json({ accesstoken });
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
     },
     logout: async (req, res) => {
         try {
-            res.clearCookie("refreshtoken", { path: "/user/refresh_token" });
+            res.clearCookie("accesstoken", { path: "/user/accesstoken" });
             return res.status(400).json({ msg: "Logout" });
         } catch (error) {
             return res.status(500).json({ msg: error.message })
@@ -66,8 +66,8 @@ const userCtrl = {
     getUser: async (req, res) => {
         try {
             const user = await User.findById(req.user.id).select("-password");
-            if (!user) 
-            return res.status(400).json({ msg: "User does not exists" });
+            if (!user)
+                return res.status(400).json({ msg: "User does not exists" });
             res.json({ user });
         } catch (error) {
             return res.status(500).json({ msg: error.message })
@@ -77,7 +77,7 @@ const userCtrl = {
 const createAccessToken = (user) => {
     return jwt.sign(user, process.env.ACCESS, { expiresIn: "1d" });
 };
-const createRefreshToken = (user) => {
-    return jwt.sign(user, process.env.REFRESH, { expiresIn: "1d" });
-};
+// const createRefreshToken = (user) => {
+//     return jwt.sign(user, process.env.REFRESH, { expiresIn: "1d" });
+// };
 module.exports = userCtrl;
